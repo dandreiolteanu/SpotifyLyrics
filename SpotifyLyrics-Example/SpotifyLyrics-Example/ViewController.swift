fdf636dabdb5aa7a3fc0f6eaba7b8b5527070120
Andrei Olteanu <a.olteanu198@yahoo.com>
2019-02-13 22:55:41 +0200

    add Xcode project

---
+++ SpotifyLyrics-Example/SpotifyLyrics-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyLyrics-Example
+//
+//  Created by Olteanu Andrei on 13/02/2019.
+//  Copyright © 2019 Andrei Olteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
